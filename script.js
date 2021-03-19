e003f9f39383638fbf093bdbb459476f94ac1382
partum <jarringt@uccs.edu>
2021-03-19 13:29:44 -0600

    added scroll to top button

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function animation_start(id) {
 function animation_stop(id) {
   document.getElementById(id).style.opacity = "0";
 }
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
